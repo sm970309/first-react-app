6b3e2f2d66591b4deb5d7bd21fe7a110e8f539e7
sm970309 <sm970309@gmail.com>
2022-04-07 11:12:45 +0900

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
   const [value,setValue] = useState(0)
   const onClick = () => setValue((prev) => prev+1);
+  console.log('i run all the time')
+  useEffect(() =>{
+    console.log('i run only once.');
+  },[])
   return (
     <div className="App">
       <h1 className={styles.title}>Welcome Back!</h1>
