72f7a1d7cc1523b9403dd82e763506a3add1f4b9
sm970309 <sm970309@gmail.com>
2022-04-07 11:33:56 +0900

    useEffect details

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 function App() {
-  const [value,setValue] = useState(0)
+  const [value,setValue] = useState(0);
+  const [keyword,setKeyword] = useState("")
+  const onChange=(event) => {
+    setKeyword(event.target.value);
+  }
   const onClick = () => setValue((prev) => prev+1);
-  console.log('i run all the time')
+  // console.log('i run all the time')
   useEffect(() =>{
-    console.log('i run only once.');
+    console.log('CALL TEH API...');
   },[])
+  useEffect(() =>{
+    if (keyword!="" && keyword.length>5){
+    console.log('SERCH FOR',keyword);
+    }
+  },[keyword])
+  useEffect(() =>{
+    console.log("value is changed.");
+  },[value])
   return (
     <div className="App">
+    <input value={keyword} onChange={onChange}type="text" placeholder = "Search here..."></input>
       <h1 className={styles.title}>Welcome Back!</h1>
       <h2>{value}</h2>
       <Button text={"Click me"} onClick = {onClick} />
