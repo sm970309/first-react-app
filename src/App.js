1e4526c8703cefda9458a310c000f283856823a5
sm970309 <sm970309@gmail.com>
2022-04-07 10:34:36 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 function App() {
+  const [value,setValue] = useState(0)
+  const onClick = () => setValue((prev) => prev+1);
   return (
     <div className="App">
       <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <h2>{value}</h2>
+      <Button text={"Click me"} onClick = {onClick} />
     </div>
   );
 }
